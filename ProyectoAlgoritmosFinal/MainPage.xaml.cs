f9e023b4a79cc5fb6eafd419ef159e3b7a9e750e
HP <HP@DESKTOP-441JAVA>
2016-05-16 21:14:00 -0500

    -Diseño Log In -Creacion buttons

---
+++ ProyectoAlgoritmosFinal/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ProyectoAlgoritmosFinal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void logIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void logInbtn_Click(object sender, RoutedEventArgs e)
+        {
+            logInfrm.Visibility = Visibility.Visible;
+        }
+
+        private void enviarFrm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cancerlarFrm_Click(object sender, RoutedEventArgs e)
+        {
+            logInfrm.Visibility = Visibility.Collapsed;
+        }
+    }
+}
